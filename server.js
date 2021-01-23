f0958be8d4209c08b672179698817647ad8bda7d
GSCrawley <gscrawley@gmail.com>
2021-01-23 02:19:55 -0800

    deploying to heroku

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
