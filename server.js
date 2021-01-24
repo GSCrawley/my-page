8eac04243505799e92a1bc67af9e76ba19dbd242
GSCrawley <gscrawley@gmail.com>
2021-01-23 17:16:21 -0800

    deployed but still getting Cannot GET / message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port == null || port === "") {
   port = 8000;
 }
 app.listen(port);
