67400541ed43d28716936e8cd24424e7bd012c54
GSCrawley <gscrawley@gmail.com>
2021-01-23 18:02:18 -0800

    ..

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
-
+    
   }
 
   getResumeData(){
