195242928effe996c8e7d68c812a90da35bde9c2
GSCrawley <gscrawley@gmail.com>
2021-01-23 18:15:07 -0800

    changed app use to dirname + gideoncrawleyherokuapp in server file hopefully that does the trick

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + 'https://gideoncrawley.herokuapp.com/'));
 let port = process.env.PORT;
 if (port == null || port === "") {
   port = 8000;
