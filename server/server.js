ac734df39eddb436f7fc333587d84759e0186c65
GSCrawley <gscrawley@gmail.com>
2021-01-23 18:54:04 -0800

    put build folder in server folder, lets hope it works

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname, 'build'));
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname, 'build', 'index.html');
+});
+
 let port = process.env.PORT;
 if (port == null || port === "") {
   port = 8000;
