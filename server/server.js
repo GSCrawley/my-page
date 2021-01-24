fe0ac70760f97ef907e9b0b0c970bdc61df08a09
GSCrawley <gscrawley@gmail.com>
2021-01-23 18:57:23 -0800

    ..

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname, 'build'));
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname, 'build', 'index.html');
 });
 
